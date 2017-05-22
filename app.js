8b346dafa80bbf5a5c2c22234dd4fb66f59b2808
Richard Neil Ilagan <richard.ilagan@hpe.com>
2017-05-22 14:31:52 +0100

    Creates XLSX successfully

--- app.js
+++ app.js
@@ -3,26 +3,16 @@ const each = require('lodash/each')
 
 const filesService = require('./lib/files-service')
 const manifestService = require('./lib/manifest-service')
+const worksheetService = require('./lib/worksheet-service')
 
 filesService.getSetDefinitions()
   .catch(err => {
     console.error(err)
   })
-  .then(files => {
-    return Promise.all(
-      files.map(f =>
-        manifestService.getSongManifest(f)
-          .then(manifest => manifestService.getSongMetadata(manifest))
-      )
+  .then(files => Promise.all(
+    files.map(f => manifestService
+      .getSongManifest(f)
+      .then(manifest => manifestService.getSongMetadata(manifest))
     )
-  })
-  .then(metas => {
-    const metasByDir = groupBy(metas, m => m.dir)
-    each(metasByDir, (files, dir) => {
-      console.log(`-- :: ${dir} :: --`)
-
-      each(files, file => {
-        console.log(file.title, file.author, file.comments || '')
-      })
-    })
-  })
+  ))
+  .then(metas => worksheetService.exportToWorksheet(metas))
