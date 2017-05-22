462b6ddbd9a0dca73631e7ef425d6716e69e1ff3
Richard Neil Ilagan <richard.ilagan@hpe.com>
2017-05-22 16:53:46 +0100

    Adds logging

--- app.js
+++ app.js
@@ -4,15 +4,28 @@ const each = require('lodash/each')
 const filesService = require('./lib/files-service')
 const manifestService = require('./lib/manifest-service')
 const worksheetService = require('./lib/worksheet-service')
+const logger = require('./lib/logging-service')
 
 filesService.getSetDefinitions()
   .catch(err => {
     console.error(err)
   })
-  .then(files => Promise.all(
-    files.map(f => manifestService
-      .getSongManifest(f)
-      .then(manifest => manifestService.getSongMetadata(manifest))
-    )
-  ))
+  .then(files => {
+    const task = logger.add('Reading manifests')
+    let reduce = 0
+
+    return Promise.all(
+      files.map(f => manifestService
+        .getSongManifest(f)
+        .then(manifest => {
+          task.details(`${++reduce} manifests completed.`)
+          return manifest
+        })
+        .then(manifest => manifestService.getSongMetadata(manifest))
+      )
+    ).then(metas => {
+      task.done()
+      return metas
+    })
+  })
   .then(metas => worksheetService.exportToWorksheet(metas))
